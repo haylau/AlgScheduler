e8ae633b527ecaada7fb2da165bd91339bd44936
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-02-09 04:00:49 -0800

    update sh / output is correctgit add .

--- run.sh
+++ run.sh
@@ -6,31 +6,31 @@ make rr
 make priority
 make priority_rr
 
-echo -e "\nRunning FCFS" 
+echo -e "\n-- Running FCFS --" 
 ./bin/fcfs tests/book.txt
 echo -e "\n"
 ./bin/fcfs tests/schedule.txt
 echo -e "\n"
 
-echo -e "Running SJF" 
+echo -e "-- Running SJF --" 
 ./bin/sjf tests/book.txt
 echo -e "\n"
 ./bin/sjf tests/schedule.txt
 echo -e "\n"
 
-echo -e "Running RR" 
+echo -e "-- Running RR --" 
 ./bin/rr tests/schedule.txt
 echo -e "\n"
 ./bin/rr tests/rr-schedule.txt
 echo -e "\n"
 
-echo -e "Running PRI" 
+echo -e "-- Running PRI --" 
 ./bin/priority tests/schedule.txt
 echo -e "\n"
 ./bin/priority tests/pri-schedule.txt
 echo -e "\n"
 
-echo -e "Running RR-PRI" 
+echo -e "-- Running RR-PRI --" 
 ./bin/priority_rr tests/rr-schedule.txt
 echo -e "\n"
 ./bin/priority_rr tests/schedule.txt
